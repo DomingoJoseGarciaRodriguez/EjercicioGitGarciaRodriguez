a7ca5834b180b067b9579dad977c6396a57daba1
Domingo Garcia <1408409@alu.murciaeduca.es>
2023-05-25 19:16:22 +0200

    Confirmacion proyecto GarciaRodriguez

---
+++ src/main/java/com/mycompany/garciarodriguez/GarciaRodriguez.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.garciarodriguez;
+
+/**
+ *
+ * @author garci
+ */
+public class GarciaRodriguez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
